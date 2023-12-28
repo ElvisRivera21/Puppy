39cac8eb6a85df77d54bf3649cfa43734bf3504a
Elvis Rivera <therealelvisrivera@gmail.com>
2023-12-28 16:54:46 -0600

    array commit

--- App.jsx
+++ App.jsx
@@ -1,38 +1,42 @@
-import { useState } from 'react'
-import { puppyList } from './data.js'
-
-
-
-import './App.css'
+import React, { useState } from 'react';
+import { puppyList } from './data.js';
+import './App.css';
 
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
 
-  console.log("puppyList: ", puppyList);
+  const handlePuppyClick = (id) => {
+    setFeatPupId(id);
+  };
 
-  function handleClick() {
-   
-  }
-
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
   return (
     <>
       <div className="App">
-        {puppies.map((puppy) => {
-          return (
-            <p onClick={() => { console.log("puppy id: ", puppy.id) }} key={puppy.id}>
-              {puppy.name}
-            </p>
-          );
-        })}
+        {puppies.map((puppy) => (
+          <p onClick={() => handlePuppyClick(puppy.id)} key={puppy.id}>
+            {puppy.name}
+          </p>
+        ))}
       </div>
-      
-     
-  
+
+      {/* Display selected puppy information */}
+      {featPupId && <p>Selected Puppy ID: {featPupId}</p>}
+
+      <div>
+        {featuredPup && (
+          <>
+            <h2>{featuredPup.name}</h2>
+            <p>Breed: {featuredPup.breed}</p>
+            <p>Age: {featuredPup.age}</p>
+            {/* Add more details as needed */}
+          </>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
